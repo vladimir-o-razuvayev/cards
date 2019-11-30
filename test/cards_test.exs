8df6e80f5304e96e88abb9f9e5bc68a9fce93448
Vladimir O. Razuvayev <vladimir.o.razuvayev@gmail.com>
2019-11-29 20:51:55 -0500

    fixed typo in test description

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,7 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "create_deck makes 20 cards" do
+  test "create_deck makes 52 cards" do
       assert length(Cards.create_deck) == 52
   end
 end
